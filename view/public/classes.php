5050e8604534799fa5d9f54611963d6ae1a5d899
teemo24 <tounsi.online@gmail.com>
2022-10-01 19:58:23 +0100

    Update classes.php

--- view/public/classes.php
+++ view/public/classes.php
@@ -28,16 +28,16 @@
 				<div class="classess-block col-lg-4 col-md-6 col-sm-12">
 					<div class="inner-box">
 						<div class="image">
-							<a href="classes/class"><img src="assets/images/resource/feature-1.jpg" alt="" /></a>
+							<a href="class"><img src="assets/images/resource/feature-1.jpg" alt="" /></a>
 							<div class="icon-box">
 								<span class="icon flaticon-tools-and-utensils-1"></span>
 							</div>
 						</div>
 						<div class="lower-content">
 							<div class="title">GYM</div>
-							<h4><a href="classes/class">Psycho Training</a></h4>
+							<h4><a href="class">Psycho Training</a></h4>
 							<div class="text">Winning means you’re willing to go longer, work harder for more.</div>
-							<a class="read-more" href="classes/class">Read More</a>
+							<a class="read-more" href="class">Read More</a>
 						</div>
 					</div>
 				</div>
@@ -46,16 +46,16 @@
 				<div class="classess-block col-lg-4 col-md-6 col-sm-12">
 					<div class="inner-box">
 						<div class="image">
-							<a href="classes/class"><img src="assets/images/resource/feature-2.jpg" alt="" /></a>
+							<a href="class"><img src="assets/images/resource/feature-2.jpg" alt="" /></a>
 							<div class="icon-box">
 								<span class="icon flaticon-muscle"></span>
 							</div>
 						</div>
 						<div class="lower-content">
 							<div class="title">CARDIO</div>
-							<h4><a href="classes/class">Self Defence</a></h4>
+							<h4><a href="class">Self Defence</a></h4>
 							<div class="text">Don’t practice until you get it right practice until can’t wrong</div>
-							<a class="read-more" href="classes/class">Read More</a>
+							<a class="read-more" href="class">Read More</a>
 						</div>
 					</div>
 				</div>
@@ -64,16 +64,16 @@
 				<div class="classess-block col-lg-4 col-md-6 col-sm-12">
 					<div class="inner-box">
 						<div class="image">
-							<a href="classes/class"><img src="assets/images/resource/feature-3.jpg" alt="" /></a>
+							<a href="class"><img src="assets/images/resource/feature-3.jpg" alt="" /></a>
 							<div class="icon-box">
 								<span class="icon flaticon-people"></span>
 							</div>
 						</div>
 						<div class="lower-content">
 							<div class="title">Fitness</div>
-							<h4><a href="classes/class">FITNESS FOR MAN</a></h4>
+							<h4><a href="class">FITNESS FOR MAN</a></h4>
 							<div class="text">Winning means you’re willing to go longer, work harder for more.</div>
-							<a class="read-more" href="classes/class">Read More</a>
+							<a class="read-more" href="class">Read More</a>
 						</div>
 					</div>
 				</div>
@@ -82,16 +82,16 @@
 				<div class="classess-block col-lg-4 col-md-6 col-sm-12">
 					<div class="inner-box">
 						<div class="image">
-							<a href="classes/class"><img src="assets/images/resource/feature-4.jpg" alt="" /></a>
+							<a href="class"><img src="assets/images/resource/feature-4.jpg" alt="" /></a>
 							<div class="icon-box">
 								<span class="icon flaticon-fitness"></span>
 							</div>
 						</div>
 						<div class="lower-content">
 							<div class="title">STRENGTH</div>
-							<h4><a href="classes/class">Strength Training</a></h4>
+							<h4><a href="class">Strength Training</a></h4>
 							<div class="text">Winning means you’re willing to go longer, work harder for more.</div>
-							<a class="read-more" href="classes/class">Read More</a>
+							<a class="read-more" href="class">Read More</a>
 						</div>
 					</div>
 				</div>
@@ -100,16 +100,16 @@
 				<div class="classess-block col-lg-4 col-md-6 col-sm-12">
 					<div class="inner-box">
 						<div class="image">
-							<a href="classes/class"><img src="assets/images/resource/feature-5.jpg" alt="" /></a>
+							<a href="class"><img src="assets/images/resource/feature-5.jpg" alt="" /></a>
 							<div class="icon-box">
 								<span class="icon flaticon-machine"></span>
 							</div>
 						</div>
 						<div class="lower-content">
 							<div class="title">CARDIO</div>
-							<h4><a href="classes/class">Cardio For Man</a></h4>
+							<h4><a href="class">Cardio For Man</a></h4>
 							<div class="text">Don’t practice until you get it right practice until can’t wrong</div>
-							<a class="read-more" href="classes/class">Read More</a>
+							<a class="read-more" href="class">Read More</a>
 						</div>
 					</div>
 				</div>
@@ -118,16 +118,16 @@
 				<div class="classess-block col-lg-4 col-md-6 col-sm-12">
 					<div class="inner-box">
 						<div class="image">
-							<a href="classes/class"><img src="assets/images/resource/feature-6.jpg" alt="" /></a>
+							<a href="class"><img src="assets/images/resource/feature-6.jpg" alt="" /></a>
 							<div class="icon-box">
 								<span class="icon flaticon-gym"></span>
 							</div>
 						</div>
 						<div class="lower-content">
 							<div class="title">Nutrition</div>
-							<h4><a href="classes/class">Advance GYM</a></h4>
+							<h4><a href="class">Advance GYM</a></h4>
 							<div class="text">Winning means you’re willing to go longer, work harder for more.</div>
-							<a class="read-more" href="classes/class">Read More</a>
+							<a class="read-more" href="class">Read More</a>
 						</div>
 					</div>
 				</div>
