e62152aafa45f7be33414ff1930cebb51041afef
sayahi123 <sayahimohamed147@gmail.com>
2022-10-09 21:23:15 +0100

    updating

--- view/public/home.php
+++ view/public/home.php
@@ -2,14 +2,20 @@
 include "view/header.php";
 
 include "view/sections/slider.php";
-include "view/sections/calcul.php";
 include "view/sections/choose.php";
-include "view/sections/featured.php";
+include "view/sections/calcul.php";
+
+include "view/sections/fluid_one.php";
+
+//include "view/sections/featured.php";
 include "view/sections/counter.php";
-include "view/sections/timetable.php";
-include "view/sections/testimonial.php";
-include "view/sections/pricing.php";
-include "view/sections/news.php";
+//include "view/sections/timetable.php";
+//include "view/sections/testimonial.php";
+//
+// include"view/sections/pricing.php";
+//include "view/sections/news.php";
+include "view/sections/benefits.php";
+
 include "view/sections/clients.php";
 include "view/sections/subscribe.php";
 
--- view/sections/clients.php
+++ view/sections/clients.php
@@ -5,10 +5,11 @@
             <div class="sponsors-outer">
                 <!--Sponsors Carousel-->
                 <ul class="sponsors-carousel owl-carousel owl-theme">
-                    <li class="slide-item"><figure class="image-box"><img src="assets/images/clients/actia-logo.jpeg" alt=""></figure></li>
+                   <!-- <li class="slide-item"><figure class="image-box"><img src="assets/images/clients/actia-logo.jpeg" alt=""></figure></li>
                     <li class="slide-item"><figure class="image-box"><img src="assets/images/clients/intercom_logo.png" alt=""></figure></li>
+                    <li class="slide-item"><figure class="image-box"><img src="assets/images/clients/wevio.png" alt=""></figure></li> -->
+
                     <li class="slide-item"><figure class="image-box"><img src="assets/images/clients/sydri.png" alt=""></figure></li>
-                    <li class="slide-item"><figure class="image-box"><img src="assets/images/clients/wevio.png" alt=""></figure></li>
                     <li class="slide-item"><figure class="image-box"><img src="assets/images/clients/aman.jpeg" alt=""></figure></li>
 					<li class="slide-item"><figure class="image-box"><img src="assets/images/clients/Logo_BNA.jpg" alt=""></figure></li>
 					<li class="slide-item"><figure class="image-box"><img src="assets/images/clients/st.jpg" alt=""></figure></li>
